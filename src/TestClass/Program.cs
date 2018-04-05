7839f76f62e3959a521c20d424d0d19c1856362c
Brajesh <Brajesh@BRAJESHJAISHWAL>
2018-04-05 17:17:40 +0530

    Add project files.

---
+++ src/TestClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DesignPatterns;
+
+namespace TestClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DeviceManager dm = DeviceManager.Instance;
+        }
+    }
+}
