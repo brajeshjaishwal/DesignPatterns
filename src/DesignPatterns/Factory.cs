e83a08778fe6424168a140b00011a0d338815b30
Brajesh <Brajesh@BRAJESHJAISHWAL>
2018-04-05 18:01:44 +0530

    new patterns added

---
+++ src/DesignPatterns/Factory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    /*
+     As the defination goes:
+     Define an interface for creating an object, but let subclasses decide which class to instantiate. The Factory method lets a class defer instantiation it uses to subclasses.
+
+     We chose this pattern because we wanted to a single access point for instance creation for the class family (classes belonging to the same family).
+     */
+    public class Factory
+    {
+    }
+}
--- src/TestClass/Program.cs
+++ src/TestClass/Program.cs
@@ -11,6 +11,7 @@ namespace TestClass
         public static void Main(string[] args)
         {
             DeviceManager dm = DeviceManager.Instance;
+
         }
     }
 }
