c050c56e3f03f9af8b135dc0c840d0a57af4730b
Brajesh <Brajesh@BRAJESHJAISHWAL>
2018-04-16 16:26:21 +0530

    Facade pattern detail added

--- src/DesignPatterns/Beginner/Facade.cs
+++ src/DesignPatterns/Beginner/Facade.cs
@@ -5,7 +5,31 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
-    public class Facade
+    /*
+     * A facade is an object that provides a simplified interface to a larger body of code.
+     * 
+     * We chose to use this pattern in our project because we wanted to have a single interface window to a complex module.
+     */
+    public class ChromScopeSystem
     {
+        // instances of various modules
+        private IConfigurationManager _configManager;
+        private ICollectionManager _collectionManager;
+        private IDataFileManager _dataFileManager;
+        private IWorkflowManager _workFlowManager;
+
+        /*
+         * Work flow manager related methods
+         */
+
+        /*
+         * Collection manager related methods
+         */
+
+
     }
+
+    public interface IConfigurationManager { }
+    public interface IDataFileManager { }
+    public interface IWorkflowManager { }
 }
