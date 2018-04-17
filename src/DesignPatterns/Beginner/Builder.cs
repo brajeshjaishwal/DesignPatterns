7620b0d2b4c20f333cba5800d16991ad64afc106
Brajesh <Brajesh@BRAJESHJAISHWAL>
2018-04-17 12:25:06 +0530

    Added detail to builder pattern

--- src/DesignPatterns/Beginner/Builder.cs
+++ src/DesignPatterns/Beginner/Builder.cs
@@ -5,7 +5,105 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
-    public class Builder
+    /*
+     * Separate the construction of a complex object from its representation so that the
+     * same construction process (Director) can create different representations (Builders).
+     * 
+     * 
+     */
+
+    /// <summary>
+    /// Same construction process
+    /// </summary>
+    public class CommandDirector
     {
+        public void Construct(IBuilder b)
+        {
+            b.SetId();
+        }
     }
+
+    public interface IBuilder
+    {
+        void SetId();
+        Command GetResult();
+    }
+
+    /// <summary>
+    /// Representation 1 of the same product
+    /// </summary>
+    public class LoadIntegrationCommandBuilder : IBuilder
+    {
+        Command p = new Command();
+        public void SetId()
+        {
+            p.SetId(CommandId.LoadIntegration);
+        }
+        public Command GetResult()
+        {
+            return p;
+        }
+    }
+
+    /// <summary>
+    /// Representation 2 of the same product
+    /// </summary>
+    public class ManualIntegrationCommandBuilder : IBuilder
+    {
+        Command p = new Command();
+        public void SetId()
+        {
+            p.SetId(CommandId.ManualIntegration);
+        }
+        public Command GetResult()
+        {
+            return p;
+        }
+    }
+
+    public class Command
+    {
+        public CommandId Id;
+
+        public void SetId(CommandId id)
+        {
+            Id = id;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\nFeature list ...");
+            Console.WriteLine($"Command id: {Id}");
+            Console.WriteLine();
+        }
+    }
+
+    public class Client
+    {
+        public static void Main()
+        {
+            // Create one director means same construction process
+            CommandDirector director = new CommandDirector();
+
+            //Create builders means same product but different representations
+            IBuilder b1 = new LoadIntegrationCommandBuilder();
+            IBuilder b2 = new ManualIntegrationCommandBuilder();
+
+            // Construct two products
+            director.Construct(b1);
+
+            //representation 1
+            Command p1 = b1.GetResult();
+            p1.Display();
+
+            director.Construct(b2);
+            //representation 2
+            Command p2 = b2.GetResult();
+            p2.Display();
+
+            Console.Read();
+        }
+    }
+
+    public enum CommandId { LoadIntegration, ManualIntegration }
 }
