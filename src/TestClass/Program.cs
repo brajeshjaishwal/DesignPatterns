d40b003fcb3c6609557a4e53b8357bdad5b82591
brajeshjaishwal <brajesh.jaishwal@gmail.com>
2018-05-21 18:00:29 +0530

    migrated to new dotnet version

--- src/TestClass/Program.cs
+++ src/TestClass/Program.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DesignPatterns;
-
-namespace TestClass
+﻿namespace TestClass
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            /* 
             DeviceManagerSingleton dm = DeviceManagerSingleton.Instance;
             IDeviceFactory adcf = AbstractDeviceCreationFactory.Instance;
             ICollectionManagerFactory icmf = new CollectionManagerFactory();
             ICollectionManager obcm = icmf.GetCollectionManager(DeviceSubGroup.OpenbedCollector);
             ICollectionManager cbcm = icmf.GetCollectionManager(DeviceSubGroup.ClosedbedCollector);
+            */
         }
     }
 }
