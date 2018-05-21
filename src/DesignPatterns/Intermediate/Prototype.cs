65a311f3d632b5b7987b3935f3bd56abb4a9c161
brajeshjaishwal <brajesh.jaishwal@gmail.com>
2018-05-21 18:18:14 +0530

    "Moved prototype pattern from beginner to intermediate category"

--- src/DesignPatterns/Intermediate/Prototype.cs
+++ src/DesignPatterns/Intermediate/Prototype.cs
@@ -1,11 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BinaryFormatter;
 
 namespace DesignPatterns.Intermediate
 {
-    public class Prototype
+    /*
+     * This pattern allows an object to create customized objects without knowing their class or any details of how to create them. 
+     * 
+     * This can be achieved by
+     * cloning (shallow or deep, based on requirement), 
+     * copy constructor or
+     * serialization/deserialization approach
+     */
+    public static class CloneManager
     {
+        public static T DeepCopy<T>(this T t)
+        {
+            var formatter = new BinaryConverter();
+            byte [] raw = formatter.Serialize(t);
+            return formatter.Deserialize<T>(raw);
+        }
+    }
+
+    [Serializable]
+    public class Address
+    {
+        public string [] Names = new string[]{"first", "second"};
+        public string PinCode = "302029";
+        
+        public Address() {}
     }
 }
