f83fb6e1004e21051cb48dd88da245fa2a9c9490
brajeshjaishwal <brajesh.jaishwal@gmail.com>
2018-06-05 13:04:39 +0530

    .

--- src/DesignPatterns/Intermediate/Mediater.cs
+++ src/DesignPatterns/Intermediate/Mediater.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 namespace DesignPatterns.Intermediate
 {
     /// <summary>
-    /// Definition: Define an object that encapsulates how a set of objects interact. 
+    /// Definition: Define an object that encapsulates how a set of objects
+    ///             interact or communicate with each other. 
+    ///
     /// Mediator promotes loose coupling by keeping objects from referring to each other explicitly, 
     /// and it lets you vary their interaction independently. 
     /// 
